4b22df5cbe18ab491471567f384897e8b393e658
Camila Zelenovsky <camila_zele@hotmail.com>
2020-08-03 21:47:39 -0300

    fist commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que número deseja somar?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int soma = 0;
+
+            while (number >0)
+            {
+                soma += number % 10;
+                number /= 10;
+            }
+
+            Console.WriteLine($"A soma dos números é {soma}.");
+        }
+    }
+}
